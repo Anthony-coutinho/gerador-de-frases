89dab1a9f9591c699777cf3f445ec63a40c6b6b3
Antony <antony@gmail.com>
2025-11-07 18:34:45 -0300

    fix and feat: bug do tailwind corrigido - consumindo api de frases

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import PhraseGenerator from "./Components/PhraseGenerator.js";
 
 function App() {
   return (
-    <div className="flex flex-col font-serif gap-4 bg-gradent-to-r from-teal-500 to-sky-500 to-sky-500 h-screenp-10 text-neutral-800">
-      <div className="bg-white ronded-xl p-4 shadow-lg">
+    <div className="flex flex-col font-serif gap-4 bg-gradient-to-r from-teal-500 to-sky-500 to-sky-500 h-screen p-10 text-neutral-800">
+      <div className="bg-white rounded-xl p-4 shadow-lg">
         <h1 className="text-lg">Gerador de Frases</h1>
         <hr className="h-px my-3"/>
         
--- src/Components/PhraseGenerator.js
+++ src/Components/PhraseGenerator.js
@@ -1,17 +1,58 @@
 import React, { useState } from "react";
 import Phrase from "./Phrase.js";
+import axios from "axios";
 
 export default function PhraseGenerator() {
-    const [PhraseA, setPhraseA] = useState({ content: "Uma vida não examinada não vale ser vivida", author: "Sócrates" })
+    const [PhraseA, setPhraseA] = useState({ content: "", author: "" })
+
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    
+
+    const getRandomPhrase = async () => {
+        try {
+            setLoading (true);
+            const response = await axios.get("https://api.quotable.io/random");
+            const data = response.data;
+            setPhraseA({
+                content: data.content,
+                author: data.author,
+            });
+        } catch (error) {
+            setError("Ocorreu um Erro")
+            console.error("Ocorreu um Erro", error);
+    } finally {
+        setLoading(false);
+    }
+
+    if (loading){
+        return (
+            <div className="min-h20">
+                <p className="text-lg">loading...</p>
+            </div>
+        );
+    }
+
+    if (error) {
+        return (
+        <div className="min-h-20">
+            <p className="text-lg">{error}</p>
+        </div>
+        );
+    }
+}
 
 return (
     <div className="min-h-20 flex flex-col gap-4 items-start">
-        <Phrase text={PhraseA.content} PhraseAuthor={PhraseA.author} />
+        <Phrase text={PhraseA.content} author={PhraseA.author} />
 
         <button
-        className="bg-gradient-to-r from-teal-500 to-sky-500 p-2 rounded-lg shadow-md text-white">
+        className="bg-gradient-to-r from-teal-500 to-sky-500 p-2 rounded-lg shadow-md text-white"
+        onClick={getRandomPhrase}
+        >
             Nova Frase 
         </button>
 
     </div>
 )}
+
